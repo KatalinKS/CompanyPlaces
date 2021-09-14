df15d01ddb7243ff4d6c117e5cfb0bd012256d4c
Skej <konstantin@katalin.ru>
2021-09-14 11:47:45 +0300

    Добавлен интерфейс фасада

---
+++ src/Interfaces/CompanyPlaces.php
@@ -0,0 +1,9 @@
+<?php
+namespace KatalinKS\CompanyPlaces\Interfaces;
+
+use KatalinKS\CompanyPlaces\Models\CompanyPlace;
+
+interface CompanyPlaces
+{
+    public function getProcessingOffice(): CompanyPlace;
+}
